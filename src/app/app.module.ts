98094bb8c08ea45eaeae7b0d60e64f4567bc4059
joaolucasMota <joaolucasmmd@gmail.com>
2023-05-29 12:22:22 -0300

    att 29/05

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { ToolbarComponent } from './commom/components/toolbar/toolbar.component';
 import { MaterialModule } from './shared/material/material.module';
 import { FormularioComponent } from './features/entradas/components/formulario/formulario.component';
 import localePt from '@angular/common/locales/pt';
 import { LOCALE_ID } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
+import { AuthGuard } from './commom/auth.guard';
+import { AuthInterceptor } from './commom/auth.interceptor';
 
 registerLocaleData(localePt);
 @NgModule({
@@ -29,6 +31,8 @@ registerLocaleData(localePt);
     ReactiveFormsModule,
   ],
   providers: [
+    AuthGuard,
+    { provide : HTTP_INTERCEPTORS, useClass : AuthInterceptor, multi: true},
     { provide: LOCALE_ID, useValue: 'pt-BR' },
   ],
   bootstrap: [AppComponent]
